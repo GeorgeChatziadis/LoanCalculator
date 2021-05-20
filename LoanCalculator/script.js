bc22d49a3c8c3c2d2fed0bdcebd33d645b80538e
GeorgeChatziadis <73250242+GeorgeChatziadis@users.noreply.github.com>
2021-05-20 19:38:39 +0300

    Add files via upload

---
+++ LoanCalculator/script.js
@@ -0,0 +1,9 @@
+﻿function computeLoan() {
+    var amount = document.getElementById('amount').value;
+    var interest_rate = document.getElementById('interest_rate').value;
+    var months = document.getElementById('months').value;
+    var interest = (amount * (interest_rate * 0.01)) / months;
+    var payment = ((amount / months) + interest).toFixed(2); //two numbers after the decimal
+    payment = payment.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); //puts a comma every three digits
+    document.getElementById('payment').innerHTML = 'Monthly Payment = $' + payment;
+}
